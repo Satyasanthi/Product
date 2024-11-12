2a10f29993f822e7f664b4b2f706de5b2191f436
satya <satya@gmail.com>
2024-11-12 14:23:53 +0530

    backend

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+// db.js (Database connection setup)
+const { Pool } = require('pg');
+
+// PostgreSQL connection
+const pool = new Pool({
+  user: 'postgres', // Your PostgreSQL username
+  host: 'localhost', // If using local setup
+  database: 'Buyer', // Your PostgreSQL database name
+  password: 'Satya@123', // Your PostgreSQL password
+  port: 5432, // Default PostgreSQL port
+});
+
+module.exports = pool;
